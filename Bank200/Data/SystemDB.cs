e64b78d07dc6c47ebfcdb5c5f10ebf00c69eefef
WebDeveloperDesigner2022 <4275876@myuwc.ac.za>
2022-08-04 16:20:04 +0200

    Initialize data on SystemDB

--- Bank200/Data/SystemDB.cs
+++ Bank200/Data/SystemDB.cs
@@ -4,9 +4,28 @@
     {
         //
         public List<string> currentAccount;
-        public SystemDB()
+        private int _balance;
+        private int _overdraft;
+        private int _customerNumber;
+        public SystemDB(int customerNumber, int balance, int overdraft)
         {
+            SavingAccount(customerNumber = 1, balance = 2000);
+            SavingAccount(customerNumber = 2, balance = 5000);
+            CurrentAccount(customerNumber = 3, balance = 1000, overdraft = 10000);
+            CurrentAccount(customerNumber = 4, balance = 5000, overdraft = 10000);
+            _balance = balance;
+            _overdraft = overdraft;
+            _customerNumber = customerNumber;
+        }
 
+        public void CurrentAccount(int value1, int value2, int value3)
+        {
+            
+        }
+
+        public void SavingAccount(int value1, int value2)
+        {
+            
         }
     }
 }
